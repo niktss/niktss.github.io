2022c93d345ba17404eb15b67eb876b850acaf0d
Nik <57101103+Frivolous2@users.noreply.github.com>
2023-07-25 05:43:54 +0100

    edit metadata.js

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
-	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	title: "Nik's Blog",
+	url: "https://nik.blog/",
+	language: "en-GB",
+	description: "Hot takes only, fresh from the microwave! If you have any complaints, please go to helen.dontkombach@nik.blog",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Nik",
+		email: "helen.dontkombach@nik.blog",
+		url: "https://nik.blog/whoami/"
 	}
 }
