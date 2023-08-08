ee15c849fbfc3fd0ce0f440fa44a3b5ae22e9ed2
Nik Tsonchev <nikola.tss@protonmail.ch>
2023-08-08 17:54:38 +0300

    remove unused stylesheets

--- eleventy.config.js
+++ eleventy.config.js
@@ -14,7 +14,6 @@ const pluginImages = require("./eleventy.config.images.js");
 module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
-	eleventyConfig.addPassthroughCopy("./node_modules/prismjs/themes/prism-okaidia.css");
 	eleventyConfig.addPassthroughCopy("src/assets");
 
 	// Run Eleventy when these files change:
